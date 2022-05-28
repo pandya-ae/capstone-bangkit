f58422ab4801f9007ad89cac5be9936939e7abea
ByteZEE <muhammadrafiadly@gmail.com>
2022-05-28 16:00:12 +0700

    auth view

---
+++ MD/DocPlant/app/src/main/java/com/dicoding/picodiploma/docplant/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.docplant.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.picodiploma.docplant.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
