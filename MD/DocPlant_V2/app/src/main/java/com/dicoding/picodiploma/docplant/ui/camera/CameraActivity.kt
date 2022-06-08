20e9b728137fd8817e7472bbc8a04b868d825c5e
ByteZEE <muhammadrafiadly@gmail.com>
2022-06-08 14:51:21 +0700

    add camera x and gallery

--- MD/DocPlant_V2/app/src/main/java/com/dicoding/picodiploma/docplant/ui/camera/CameraActivity.kt
+++ MD/DocPlant_V2/app/src/main/java/com/dicoding/picodiploma/docplant/ui/camera/CameraActivity.kt
@@ -1,12 +1,115 @@
 package com.dicoding.picodiploma.docplant.ui.camera
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dicoding.picodiploma.docplant.R
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.dicoding.picodiploma.docplant.databinding.ActivityCameraBinding
+import com.dicoding.picodiploma.docplant.ui.camera.cameraX.CustomCameraActivity
+import com.dicoding.picodiploma.docplant.utils.rotateBitmap
+import com.dicoding.picodiploma.docplant.utils.uriToFile
+import java.io.File
 
 class CameraActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCameraBinding
+    private var getFile: File? = null
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_camera)
+        binding = ActivityCameraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+        setupAction()
+    }
+
+    private fun setupAction() {
+        binding.apply {
+            btnCamera.setOnClickListener { startCameraX() }
+            btnGallery.setOnClickListener { startGallery() }
+        }
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = uriToFile(selectedImg, this@CameraActivity)
+            getFile = myFile
+
+        }
+    }
+
+    private fun startCameraX() {
+        val intent = Intent(this, CustomCameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = it.data?.getSerializableExtra("picture") as File
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+
+            val result = rotateBitmap(
+                BitmapFactory.decodeFile(myFile.path),
+                isBackCamera
+            )
+
+            binding.previewImageView.setImageBitmap(result)
+        }
     }
 }
