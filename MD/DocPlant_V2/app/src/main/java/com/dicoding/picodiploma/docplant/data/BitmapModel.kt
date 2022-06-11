e02c43400381df00c7f2f7a2e54cbe7a10eca2ba
ByteZEE <muhammadrafiadly@gmail.com>
2022-06-11 20:43:44 +0700

    add scan disease feature

---
+++ MD/DocPlant_V2/app/src/main/java/com/dicoding/picodiploma/docplant/data/BitmapModel.kt
@@ -0,0 +1,11 @@
+package com.dicoding.picodiploma.docplant.data
+
+import android.graphics.Bitmap
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class BitmapModel (
+    val bitmap: Bitmap,
+    val string: String
+        ) : Parcelable
--- MD/DocPlant_V2/app/src/main/java/com/dicoding/picodiploma/docplant/ui/camera/CameraActivity.kt
+++ MD/DocPlant_V2/app/src/main/java/com/dicoding/picodiploma/docplant/ui/camera/CameraActivity.kt
@@ -3,32 +3,44 @@ package com.dicoding.picodiploma.docplant.ui.camera
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.dicoding.picodiploma.docplant.data.BitmapModel
 import com.dicoding.picodiploma.docplant.databinding.ActivityCameraBinding
+import com.dicoding.picodiploma.docplant.ml.ModelBaruFix
 import com.dicoding.picodiploma.docplant.ui.ResultActivity
 import com.dicoding.picodiploma.docplant.ui.camera.cameraX.CustomCameraActivity
 import com.dicoding.picodiploma.docplant.ui.information.InformationActivity
 import com.dicoding.picodiploma.docplant.utils.rotateBitmap
 import com.dicoding.picodiploma.docplant.utils.uriToFile
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.io.File
 
 class CameraActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCameraBinding
     private var getFile: File? = null
+    private lateinit var imageView: ImageView
+    lateinit var bitmap: Bitmap
+    var result = " "
 
     companion object {
         const val CAMERA_X_RESULT = 200
 
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
         private const val REQUEST_CODE_PERMISSIONS = 10
+        const val RESULT = " "
     }
 
     override fun onRequestPermissionsResult(
@@ -76,7 +88,13 @@ class CameraActivity : AppCompatActivity() {
             btnCamera.setOnClickListener { startCameraX() }
             btnGallery.setOnClickListener { startGallery() }
             btnUpload.setOnClickListener {
-                startActivity(Intent(this@CameraActivity, ResultActivity::class.java))
+                val diseaseResult = BitmapModel(
+                    bitmap,
+                    result
+                )
+                val moveWithObjectIntent = Intent(this@CameraActivity, ResultActivity::class.java)
+                moveWithObjectIntent.putExtra(ResultActivity.EXTRA_RESULT, diseaseResult)
+                startActivity(moveWithObjectIntent)
             }
         }
     }
@@ -86,10 +104,15 @@ class CameraActivity : AppCompatActivity() {
     ) { result ->
         if (result.resultCode == RESULT_OK) {
             val selectedImg: Uri = result.data?.data as Uri
+
             val myFile = uriToFile(selectedImg, this@CameraActivity)
             getFile = myFile
 
-            binding.previewImageView.setImageURI(selectedImg)
+//            binding.previewImageView.setImageURI(selectedImg)
+            bitmap =
+                BitmapFactory.decodeStream(contentResolver.openInputStream(selectedImg))
+            binding.previewImageView.setImageBitmap(bitmap)
+            DiseaseModel(bitmap)
         }
     }
 
@@ -120,6 +143,61 @@ class CameraActivity : AppCompatActivity() {
             )
 
             binding.previewImageView.setImageBitmap(result)
+            bitmap = result
+            DiseaseModel(bitmap)
         }
+    }
+
+    private fun DiseaseModel(bitmap: Bitmap) {
+        val name_file = "label_baru.txt"
+        val label = application.assets.open(name_file).bufferedReader().use { it.readText() }
+        val labels = label.split("\n")
+        val model = ModelBaruFix.newInstance(this)
+        var bitmapscale = Bitmap.createScaledBitmap(bitmap, 150, 150, true)
+        binding.previewImageView.setImageBitmap(bitmapscale)
+        Log.e("Bitmap", bitmapscale.toString())
+
+        // Creates inputs for reference.
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 150, 150, 3), DataType.FLOAT32)
+        val tensorImage = TensorImage(DataType.FLOAT32)
+        tensorImage.load(bitmapscale)
+
+        val byteBuffer = tensorImage.buffer
+        Log.d("shape", byteBuffer.toString())
+        Log.d("shape", inputFeature0.buffer.toString())
+        inputFeature0.loadBuffer(byteBuffer)
+
+        // Runs model inference and gets result.
+        val outputs = model.process(inputFeature0)
+        Log.e("outputs", outputs.toString())
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+        val max = getMax(outputFeature0.floatArray, outputFeature0.floatArray.size)
+        Log.e("outputGenerator: ", "-----------------------")
+        Log.e("outputGenerator: ", outputFeature0.floatArray.toList().toString())
+        Log.e("outputGenerator: ", max.toString())
+        Log.e("outputGenerator: ", outputFeature0.floatArray.size.toString())
+        Log.e("outputGenerator: ", outputFeature0.dataType.toString())
+        Log.e("outputGenerator: ", outputFeature0.dataType.toString())
+        result = labels[max]
+        model.close()
+    }
+
+    fun getMax(arr: FloatArray, size: Int): Int {
+        var ind = 0;
+        var min = 0.0f;
+
+        for (i in 0 until size) {
+            Log.e("get: ", i.toString())
+            Log.e("get i: ", arr[i].toString())
+            if (arr[i] > min) {
+                Log.e("getMax: ", i.toString())
+                Log.e("getMax: ", arr[i].toString())
+
+                min = arr[i]
+                ind = i;
+            }
+        }
+        return ind
     }
 }
